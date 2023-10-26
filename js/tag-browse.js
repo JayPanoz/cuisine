fe0ca7423c70da899089f7e4bfaa3d55af4c8f03
JayPanoz <panozpublishingteam@gmail.com>
2023-10-26 11:58:18 +0200

    Améliorations navigation par tabs

--- js/tag-browse.js
+++ js/tag-browse.js
@@ -1,16 +1,41 @@
-$('div[data-tagged]').hide();
+document.addEventListener("DOMContentLoaded", () => {
+  const tagList = document.querySelector("#tag-list");
+  tagList.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(event.target);
+    if (event.target.tagName.toLowerCase() === "a") {
+      console.log("ok check tag");
+      let el = event.target;
+      let parentEl = el.parentNode;
+      console.log(parentEl);
+      if (parentEl.hasAttribute("data-tag-label")) {
+        console.log("ok dataset");
+        if (parentEl.classList.contains("bg-accent")) {
+          console.log("accent");
+          parentEl.classList.add("bg-gray");
+          parentEl.classList.remove("bg-accent");
+          let taggedSelector = "[data-tagged='" + parentEl.dataset.tagLabel + "']";
+          let sec = document.querySelector(taggedSelector);
+          sec.classList.add("hide");
+        } else {
+          console.log("not accent")
+          parentEl.classList.add("bg-accent");
+          parentEl.classList.remove("bg-gray");
+          let taggedSelector = "[data-tagged='" + parentEl.dataset.tagLabel + "']";
+          let sec = document.querySelector(taggedSelector);
+          sec.classList.remove("hide");
+        }
+      }
+    }
+  });
 
-$('[data-tag-name]').click(function(){
-  let tag_name = $(this).data('tag-name');
-  $('div[data-tagged]').hide();
-  $('[data-tag-name]').removeClass('bg-accent').addClass('bg-gray');
-
-  let $el = $('div[data-tagged="'+tag_name+'"]');
-  $(this).addClass('bg-accent').removeClass('bg-gray');
-  $el.show();
+  if (window.location.hash.length > 1) {
+    let decodedURL = decodeURI(window.location.hash.substring(1));
+    let taggedSelector = "[data-tag-label='" + decodedURL + "'] a";
+    let el = document.querySelector(taggedSelector);
+    if (el) {
+      el.click();
+    };
+  };
 });
-
-if (window.location.hash.length > 1) {
-  let $el = $('[data-tag-name="'+window.location.hash.substring(1)+'"]');
-  $el.trigger('click');
-};
